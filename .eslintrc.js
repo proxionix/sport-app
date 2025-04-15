f51c9db0d365fa4150573590656624f692c6383c
proxionix <75951086+proxionix@users.noreply.github.com>
2025-04-15 21:54:39 +0200

    .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,9 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/vue3-recommended',
-    'eslint:recommended'
-  ],
+  parser: '@babel/eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'vue/max-attributes-per-line': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/html-self-closing': ['error', {
-      'html': {
-        'void': 'always',
-        'normal': 'always',
-        'component': 'always'
-      }
-    }]
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-env']
+    }
   }
-};
+}
